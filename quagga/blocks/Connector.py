eb2dbcea53c47b45e9cff075784a7f05dcd0c478
sergii-gavrylov <sergii.gavrylov@grammarly.com>
2015-06-21 15:19:23 +0300

    update Connectors diagram

--- quagga/blocks/Connector.py
+++ quagga/blocks/Connector.py
@@ -15,10 +15,15 @@ class Connector(object):
                                 +----------------------+
                                 |      connector       |
                                 |----------------------|    +----------------->
-     _forward_obtaining_context |  +--------------+    +----+
-    +-------------------------->|  |forward_matrix|    |_forward_usage_contexts
-                                |  +--------------+    +----+
-                                |                      |    +----------------->
+                                |  +-----------------+ +----+
+                                |  |_forward_matrices| |_forward_usage_contexts
+                                |  +-----------------+ +----+
+                                |           ^          |    +----------------->
+                                |           |          |
+     _forward_obtaining_context |   +-------+------+   |
+    +-------------------------->|   |forward_matrix|   |
+                                |   +--------------+   |
+                                |                      |
        _backward_usage_context  |  +---------------+   |
     <---------------------------+  |backward_matrix|   |
                                 |  +---------------+   |
@@ -30,69 +35,69 @@ class Connector(object):
                                 +----------------------+    +-----------------+
     """
 
-    def __init__(self, forward_matrix, forward_obtaining_context=None, backward_usage_context=None):
-        self._forward_matrices = {forward_obtaining_context.device_id: forward_matrix}
-        self._forward_obtaining_context = forward_obtaining_context
-        self._forward_usage_contexts = dict()
+    def __init__(self, f_matrix, f_obtaining_context=None, b_usage_context=None):
+        self._f_matrices = {f_obtaining_context.device_id: f_matrix}
+        self._f_obtaining_context = f_obtaining_context
+        self._f_usage_contexts = dict()
 
-        self._backward_matrices = defaultdict(dict)
-        self._backward_obtaining_contexts = dict()
-        self._backward_usage_context = backward_usage_context
+        self._b_matrices = defaultdict(dict)
+        self._b_obtaining_contexts = dict()
+        self._b_usage_context = b_usage_context
 
-    def register_user(self, user, forward_usage_context, backward_obtaining_context, backward_matrix=None):
+    def register_user(self, user, f_usage_context, b_obtaining_context, b_matrix=None):
         """
         Register user of connector's forward_matrix.
 
         :param user: block that will use this connector
-        :param forward_usage_context: context in which `forward_matrix`
+        :param f_usage_context: context in which `forward_matrix`
                                       will be used
-        :param backward_obtaining_context: context in which `backward_matrix`
+        :param b_obtaining_context: context in which `backward_matrix`
                                            of the connector will be calculated
-        :param backward_matrix: backward_matrix buffer if it is None
+        :param b_matrix: backward_matrix buffer if it is None
                                 the same as forward_matrix will be created
         """
-        usage_device_id = forward_usage_context.device_id
-        if usage_device_id != self._forward_obtaining_context.device_id and \
-                usage_device_id not in self._forward_matrices:
-            self._forward_matrices[usage_device_id] = Matrix.empty_like(self.forward_matrix, usage_device_id)
-        self._forward_usage_contexts[user] = forward_usage_context
+        u_device_id = f_usage_context.device_id
+        o_device_id = self._f_obtaining_context.device_id
+        if u_device_id != o_device_id and u_device_id not in self._f_matrices:
+            self._f_matrices[u_device_id] = Matrix.empty_like(self._f_matrices[o_device_id], u_device_id)
+        self._f_usage_contexts[user] = f_usage_context
 
-        usage_device_id = self._backward_usage_context.device_id
-        obtaining_device_id = backward_obtaining_context.device_id
-        backward_matrix = backward_matrix if backward_matrix else Matrix.empty_like(self.forward_matrix, obtaining_device_id)
-        if usage_device_id != obtaining_device_id:
-            self._backward_matrices[user][usage_device_id] = Matrix.empty_like(backward_matrix, usage_device_id)
-        self._backward_obtaining_contexts[user] = backward_obtaining_context
-        self._backward_matrices[user][obtaining_device_id] = backward_matrix
+        u_device_id = self._b_usage_context.device_id
+        o_device_id = b_obtaining_context.device_id
+        b_matrix = b_matrix if b_matrix else Matrix.empty_like(self.forward_matrix, o_device_id)
+        if u_device_id != o_device_id:
+            self._b_matrices[user][u_device_id] = Matrix.empty_like(b_matrix, u_device_id)
+        self._b_matrices[user][o_device_id] = b_matrix
+        self._b_obtaining_contexts[user] = b_obtaining_context
 
     def get_forward_matrix(self, requester=None):
-        obtaining_device_id = self._forward_obtaining_context.device_id
+        o_device_id = self._f_obtaining_context.device_id
         if not requester:
-            return self._forward_matrices[obtaining_device_id]
-        usage_device_id = self._forward_usage_contexts[requester].device_id
-        if obtaining_device_id == usage_device_id:
-            return self._forward_matrices[obtaining_device_id]
-        self._forward_matrices[obtaining_device_id].copy(self._forward_obtaining_context, self._forward_matrices[usage_device_id])
-        self._forward_usage_contexts[requester].wait(self._forward_obtaining_context)
-        return self._forward_matrices[usage_device_id]
+            return self._f_matrices[o_device_id]
+        u_device_id = self._f_usage_contexts[requester].device_id
+        if o_device_id == u_device_id:
+            return self._f_matrices[o_device_id]
+        self._f_matrices[o_device_id].copy(self._f_obtaining_context, self._f_matrices[u_device_id])
+        self._f_usage_contexts[requester].wait(self._f_obtaining_context)
+        return self._f_matrices[u_device_id]
 
     forward_matrix = property(get_forward_matrix)
 
     def get_backward_matrix(self, requester=None):
         if requester:
-            obtaining_device_id = self._backward_obtaining_contexts[requester].device_id
-            return self._backward_matrices[requester][obtaining_device_id]
-        usage_device_id = self._backward_usage_context.device_id
+            o_device_id = self._b_obtaining_contexts[requester].device_id
+            return self._b_matrices[requester][o_device_id]
+        u_device_id = self._b_usage_context.device_id
         backward_matrices = []
-        for requester, matrices in self._backward_matrices.iteritems():
-            backward_obtaining_context = self._backward_obtaining_contexts[requester]
-            obtaining_device_id = backward_obtaining_context.device_id
-            if usage_device_id != obtaining_device_id:
-                matrices[obtaining_device_id].copy(backward_obtaining_context, matrices[usage_device_id])
-                self._backward_usage_context.wait(backward_obtaining_context)
-            backward_matrices.append(matrices[usage_device_id])
+        for requester, matrices in self._b_matrices.iteritems():
+            b_obtaining_context = self._b_obtaining_contexts[requester]
+            o_device_id = b_obtaining_context.device_id
+            if u_device_id != o_device_id:
+                matrices[o_device_id].copy(b_obtaining_context, matrices[u_device_id])
+                self._b_usage_context.wait(b_obtaining_context)
+            backward_matrices.append(matrices[u_device_id])
         for backward_matrix in backward_matrices[1:]:
-            backward_matrices[0].add(self._backward_usage_context, backward_matrix)
+            backward_matrices[0].add(self._b_usage_context, backward_matrix)
         return backward_matrices[0]
 
     backward_matrix = property(get_backward_matrix)
@@ -105,8 +110,8 @@ class Connector(object):
         is done.
         """
 
-        if self._forward_obtaining_context:
-            self.forward_context.block(self._forward_usage_contexts.itervalues())
+        if self._f_obtaining_context:
+            self.forward_context.block(self._f_usage_contexts.itervalues())
 
     def wait_users(self):
         """
@@ -116,8 +121,8 @@ class Connector(object):
         is done.
         """
 
-        if self._backward_usage_context:
-            self._backward_usage_context.wait(self._backward_obtaining_contexts.itervalues())
+        if self._b_usage_context:
+            self._b_usage_context.wait(self._b_obtaining_contexts.itervalues())
 
     def __getattr__(self, name):
         attribute = getattr(self.forward_matrix, name)
