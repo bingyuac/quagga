8c322bbd6147c0ed10bd271374ba5582badf7c47
sergii-gavrylov <sergii.gavrylov@grammarly.com>
2015-09-22 12:20:47 +0300

    change Connector again :sun_with_face:

--- quagga/connector/Connector.py
+++ quagga/connector/Connector.py
@@ -1,5 +1,5 @@
 from quagga.matrix import Matrix
-from collections import defaultdict
+from quagga.context import  Context
 from quagga.matrix import SparseMatrix
 
 
@@ -36,42 +36,35 @@ class Connector(object):
                                 +----------------------+    +-----------------+
     """
 
-    def __init__(self, f_matrix, f_obtaining_context, b_usage_context=None):
-        self._f_matrices = {f_obtaining_context.device_id: f_matrix}
-        self._f_obtaining_context = f_obtaining_context
-        self._f_usage_contexts = list()
-        if b_usage_context:
+    def __init__(self, f_matrix, bu_device_id=None):
+        self._fo_device_id = f_matrix.device_id
+        self._f_matrices = {self._fo_device_id: f_matrix}
+        if bu_device_id is not None:
+            self._bu_device_id = bu_device_id
             self._b_matrices = dict()
-            self._b_usage_context = b_usage_context
-            self._b_matrices_pool = defaultdict(dict)
-            self._last_b_obtaining_context = None
+            # self._b_matrices_pool = dict()
+        self.context = Context(self._fo_device_id)
 
     @property
     def bpropagable(self):
-        return bool(self._b_usage_context)
+        return hasattr(self, '_bu_device_id')
 
-    def register_usage(self, f_usage_context, b_obtaining_context=None, accumulation_method_name=None, is_sparse=False):
+    def register_usage(self, fu_device_id, bo_device_id=None, is_sparse=False):
         """
         Register usage of connector's forward_matrix.
 
-        :param f_usage_context: context in which `forward_matrix` will be used
-        :param b_obtaining_context: context in which `backward_matrix`
+        :param fu_device_id: context in which `forward_matrix` will be used
+        :param bo_device_id: context in which `backward_matrix`
                                     of the connector will be calculated
         """
 
-        if not self._b_usage_context and b_obtaining_context:
+        if not self.bpropagable and bo_device_id:
             raise ValueError('Nobody is going to use computation from backward '
                              'step. You should not backward propagate!')
-        fu_device_id = f_usage_context.device_id
-        fo_device_id = self._f_obtaining_context.device_id
-        if fu_device_id != fo_device_id and fu_device_id not in self._f_matrices:
+        if fu_device_id != self._fo_device_id and fu_device_id not in self._f_matrices:
             self._f_matrices[fu_device_id] = Matrix.empty_like(self, fu_device_id)
-        self._f_usage_contexts.append(f_usage_context)
-        if not self._b_usage_context:
+        if not self.bpropagable:
             return self._f_matrices[fu_device_id]
-
-        bu_device_id = self._b_usage_context.device_id
-        bo_device_id = b_obtaining_context.device_id
 
         if (bo_device_id, is_sparse) not in self._b_matrices:
             if is_sparse:
@@ -79,65 +72,59 @@ class Connector(object):
             else:
                 bwd_m = Matrix.empty_like(self, bo_device_id)
             self._b_matrices[bo_device_id, is_sparse] = bwd_m
-            accumulation_method = getattr(bwd_m, accumulation_method_name)
-            setattr(bwd_m, accumulation_method_name, self.decorate_waiting(accumulation_method))
-
-        if bu_device_id != bo_device_id and (bu_device_id, is_sparse) not in self._b_matrices_pool:
+        if self._bu_device_id != bo_device_id and (self._bu_device_id, is_sparse) not in self._b_matrices_pool:
             if is_sparse:
-                bwd_m = SparseMatrix(bu_device_id)
+                bwd_m = SparseMatrix(self._bu_device_id)
             else:
-                bwd_m = Matrix.empty_like(self, bu_device_id)
-            self._b_matrices_pool[bu_device_id, is_sparse] = bwd_m
-
+                bwd_m = Matrix.empty_like(self, self._bu_device_id)
+            self._b_matrices_pool[self._bu_device_id, is_sparse] = bwd_m
         return self._f_matrices[fu_device_id], self._b_matrices[bo_device_id, is_sparse]
 
     def fprop(self):
-        o_device_id = self._f_obtaining_context.device_id
+        o_device_id = self._fo_device_id
         for u_device_id, forward_matrix in self._f_matrices.iteritems():
             if u_device_id != o_device_id:
-                self._f_matrices[o_device_id].copy_to(self._f_obtaining_context, forward_matrix)
-        self._f_obtaining_context.block(*self._f_usage_contexts)
+                forward_matrix.assign(self.context, self._f_matrices[o_device_id])
+
+        if self.bpropagable:
+            for (o_device_id, is_sparse), matrix in self._b_matrices.iteritems():
+                if is_sparse:
+                    matrix.clear()
+                else:
+                    matrix.fill(self.context, 0.0)
 
     def bprop(self):
-        if not self._b_usage_context:
+        if not self.bpropagable:
             raise ValueError('Nobody was going to use computation from backward '
                              'step. You should not backward propagate!')
-        self._last_b_obtaining_context.block(self._b_usage_context)
-        u_device_id = self._b_usage_context.device_id
+        u_device_id = self._bu_device_id
 
         for o_device_id in set(zip(*self._b_matrices.keys())[0]):
             bwd_dense = self._b_matrices.get((o_device_id, False))
             bwd_sparse = self._b_matrices.get((o_device_id, True))
             if bwd_dense:
                 if bwd_sparse:
-                    bwd_dense.add(bwd_sparse)
+                    # TODO(sergii) bwd_dense.add(context, bwd_sparse)
+                    pass
                 if u_device_id != o_device_id:
-                    bwd_dense.copy_to(self._b_usage_context, self._b_matrices_pool[u_device_id, False])
-                self._b_matrices[o_device_id, False].add(self._b_usage_context, self._b_matrices_pool[u_device_id, False])
-                bwd = self._b_matrices[u_device_id, False]
+                    self._b_matrices_pool[u_device_id, False].assign(self.context, bwd_dense)
+                    self._b_matrices[u_device_id, False].add(self.context, self._b_matrices_pool[u_device_id, False])
             else:
                 if u_device_id != o_device_id:
-                    bwd_sparse.copy_to(self._b_usage_context, self._b_matrices_pool[u_device_id, True])
-                self._b_matrices[o_device_id, True].add(self._b_usage_context, self._b_matrices_pool[u_device_id, True])
-                bwd = self._b_matrices[u_device_id, True]
-        self._last_b_obtaining_context = None
-        return bwd
+                    bwd_sparse.copy_to(self.context, self._b_matrices_pool[u_device_id, True])
+                self._b_matrices[o_device_id, True].add(self._b_matrices_pool[u_device_id, True])
+
+        if (u_device_id, False) in self._b_matrices:
+            return self._b_matrices[u_device_id, False]
+        else:
+            return self._b_matrices[u_device_id, True]
 
     backward_matrix = property(lambda self: self.bprop())
 
     def __getattr__(self, name):
-        forward_matrix = self._f_matrices[self._f_obtaining_context.device_id]
-        attribute = getattr(forward_matrix, name)
+        attribute = getattr(self._f_matrices[self._fo_device_id], name)
         if hasattr(attribute, '__call__'):
             setattr(self, name, attribute)
         else:
-            setattr(Connector, name, property(lambda self: getattr(forward_matrix, name)))
+            setattr(Connector, name, property(lambda self: getattr(self._f_matrices[self._fo_device_id], name)))
         return getattr(self, name)
-
-    def decorate_waiting(self, accumulation_method):
-        def decorated_accumulation_method(context, *args, **kwargs):
-            if self._last_b_obtaining_context:
-                context.wait(self._last_b_obtaining_context)
-            self._last_b_obtaining_context = context
-            return accumulation_method(context, args, kwargs)
-        return decorated_accumulation_method
