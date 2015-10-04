e09b2cc86ccb81663e7c1004208beb9541e84985
sergii-gavrylov <sergii.gavrylov@grammarly.com>
2015-10-04 16:21:23 +0300

    add LastSelectorBlock :hamster:

--- quagga/blocks/LastSelectorBlock.py
+++ quagga/blocks/LastSelectorBlock.py
@@ -1,53 +1,24 @@
+from itertools import izip
 from quagga.matrix import Matrix
 from quagga.context import Context
 from quagga.connector import Connector
 
 
-class SelectorBlock(object):
-    def __init__(self, x, device_id=None):
-        """
-        TODO
-        """
-        if all(e.bpropagable for e in x):
-            learning = True
-        elif all(not e.bpropagable for e in x):
-            learning = False
-        else:
-            raise ValueError('All elements of x should be bpropagable '
-                             'or non-bpropagable. Mixed state is not allowed!')
-        self.max_input_sequence_len = len(x)
+class LastSelectorBlock(object):
+    def __init__(self, x):
+        device_id = x[0].device_id
+        learning = x[0].bpropagable
         self.context = Context(device_id)
-        self.output = Matrix.empty_like(x[0], self.context.device_id)
-        self.output = Connector(self.output, self.context, self.context if learning else None)
-        self._x = x
-        self.x = []
+        self.output = Matrix.empty_like(x[0])
+        self.output = Connector(self.output, device_id if learning else None)
         if learning:
-            self.dL_dx = []
-        for e in x:
-            if learning:
-                e, dL_de = e.register_usage(self.context, self.context)
-                self.dL_dx.append(dL_de)
-            else:
-                e = e.register_usage(self.context)
-            self.x.append(e)
-        self.last_index = None
+            self.x, dL_dx = izip(*x.register_usage(device_id, device_id))
+        else:
+            self.x = x.register_usage(device_id)
 
     def fprop(self):
-        n = len(self._x)
-        if n > self.max_input_sequence_len:
-            raise ValueError('Sequence has length: {} that is too long. '
-                             'The maximum is: {}'.
-                             format(n, self.max_input_sequence_len))
-        if hasattr(self, 'dL_dx') and self.last_index is not None:
-            for i, x in enumerate(self._x):
-                if i != self.last_index:
-                    x.remove_from_deregistered_b_obtaining_contexts(self.context)
-        self.last_index = n - 1
-        self.x[self.last_index].copy_to(self.context, self.output)
+        self.output.assign(self.context, self.x[-1])
         self.output.fprop()
 
     def bprop(self):
-        for i, x in enumerate(self._x):
-            if i != self.last_index:
-                x.deregistere_b_obtaining_context(self.context)
-        self.output.backward_matrix.copy_to(self.context, self.dL_dx[self.last_index])
+        self.dL_dx[len(self.x)].add(self.output.backward_matrix)
