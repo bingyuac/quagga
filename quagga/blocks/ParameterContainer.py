4116b94773a85c42785d7f67179e1fc998da2d1b
sergii-gavrylov <sergii.gavrylov@grammarly.com>
2015-10-15 12:48:50 +0300

    add trainable option to ParameterContainer :sushi:

--- quagga/blocks/ParameterContainer.py
+++ quagga/blocks/ParameterContainer.py
@@ -5,12 +5,16 @@ from quagga.connector import Connector
 class ParameterContainer(object):
     def __init__(self, **kwargs):
         self.parameters = {}
+        self.trainable_parameters = {}
         for name, definition in kwargs.iteritems():
             device_id = definition['device_id']
             matrix = Matrix.from_npa(definition['init'](), device_id=device_id)
-            param = Connector(matrix, device_id)
+            if 'trainable' not in definition or definition['trainable']:
+                param = Connector(matrix, device_id)
+                self.trainable_parameters[name] = param
+            else:
+                param = Connector(matrix)
             self.parameters[name] = param
-        self.npa_params = {}
 
     def __getitem__(self, item):
         return self.parameters[item]
@@ -18,9 +22,3 @@ class ParameterContainer(object):
     def fprop(self):
         for param in self.parameters.itervalues():
             param.fprop()
-
-    def send_to_host(self, context, name):
-        self.npa_params[name] = self.parameters[name].to_host(context)
-
-    def get_npa_param(self, name):
-        return self.npa_params[name]
