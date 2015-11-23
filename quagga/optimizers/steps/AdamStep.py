6370ae4a1b628f050409fba82b87f86a84ae80f8
sergii-gavrylov <sergii.gavrylov@grammarly.com>
2015-11-23 19:21:11 +0200

    add AdamStep :apple:

---
+++ quagga/optimizers/steps/AdamStep.py
@@ -0,0 +1,46 @@
+import numpy as np
+import ctypes as ct
+from itertools import izip
+from quagga.matrix import Matrix
+from quagga.context import Context
+
+
+class AdamStep(object):
+    def __init__(self, parameters, learning_rate_policy, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        self.parameters = parameters
+        self.m = []
+        self.v = []
+        self.contexts = []
+        for p in self.parameters:
+            m = Matrix.empty_like(p)
+            m.sync_fill(0.0)
+            self.m.append(m)
+            v = Matrix.empty_like(p)
+            v.sync_fill(0.0)
+            self.v.append(v)
+            self.contexts.append(Context(p.device_id))
+        self.learning_rate_policy = learning_rate_policy
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.blocking_contexts = []
+        self.iteration = 0
+
+    def notify(self):
+        self.iteration += 1
+        del self.blocking_contexts[:]
+        learning_rate = ct.c_float(-self.learning_rate_policy.learning_rate)
+        learning_rate *= np.sqrt(1 - self.beta2**self.iteration) / (1 - self.beta1**self.iteration)
+
+        for p, m, v, context in izip(self.parameters, self.m, self.v, self.contexts):
+            dL_dp = p.backward_matrix
+            self.blocking_contexts.append(dL_dp.last_modification_context)
+            # m[t+1] = beta1 * m[t] + (1 - beta1) * dL_dp
+            m.scale(context, self.beta1)
+            m.add_scaled(context, 1.0 - self.beta1, dL_dp)
+
+            # v[t+1] = beta2 * v[t] + (1 - beta2) * dL_dp^2
+            v.add_scaled_hprod(context, dL_dp, dL_dp, self.beta2, (1.0 - self.beta2))
+
+            # p[t+1] = p[t] - learning_rate * m[t+1] / sqrt(v[t+1] + epsilon)
+            p.add_scaled_div_sqrt(context, learning_rate, m, v, self.epsilon)
--- quagga/optimizers/steps/__init__.py
+++ quagga/optimizers/steps/__init__.py
@@ -1,5 +1,6 @@
 from quagga.optimizers.steps.SgdStep import SgdStep
 from quagga.optimizers.steps.NagStep import NagStep
+from quagga.optimizers.steps.AdamStep import AdamStep
 from quagga.optimizers.steps.RmspropStep import RmspropStep
 from quagga.optimizers.steps.MomentumStep import MomentumStep
 from quagga.optimizers.steps.SparceSgdStep import SparseSgdStep
