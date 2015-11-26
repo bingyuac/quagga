f34cd7ed00800f220844ab9391a53a98b4b9a838
sergii-gavrylov <sergii.gavrylov@grammarly.com>
2015-11-26 21:48:29 +0200

    add Hdf5ValidationSaver :floppy_disk:

--- quagga/optimizers/observers/__init__.py
+++ quagga/optimizers/observers/__init__.py
@@ -16,3 +16,4 @@
 from quagga.optimizers.observers.Hdf5Saver import Hdf5Saver
 from quagga.optimizers.observers.ValidLossTracker import ValidLossTracker
 from quagga.optimizers.observers.TrainLossTracker import TrainLossTracker
+from quagga.optimizers.observers.Hdf5ValidationSaver import Hdf5ValidationSaver
