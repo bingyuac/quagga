1b9e9e74c498dd51202031594f54c657d4ea0325
sergii-gavrylov <sergii.gavrylov@grammarly.com>
2015-11-26 17:16:04 +0200

    add LICENSE :copyright:

--- quagga/blocks/__init__.py
+++ quagga/blocks/__init__.py
@@ -1,7 +1,25 @@
+# ----------------------------------------------------------------------------
+# Copyright 2015 Grammarly, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
 from quagga.blocks.LstmBlock import LstmBlock
+from quagga.blocks.TrickyLstmBlock import TrickyLstmBlock
+from quagga.blocks.TrickyTrickyLstmBlock import TrickyTrickyLstmBlock
 from quagga.blocks.MeanPoolingBlock import MeanPoolingBlock
 from quagga.blocks.DotBlock import DotBlock
 from quagga.blocks.SequentialMeanPoolingBlock import SequentialMeanPoolingBlock
+from quagga.blocks.SequentialSumPoolingBlock import SequentialSumPoolingBlock
 from quagga.blocks.SigmoidCeBlock import SigmoidCeBlock
 from quagga.blocks.SoftmaxCeBlock import SoftmaxCeBlock
 from quagga.blocks.NonlinearityBlock import NonlinearityBlock
@@ -14,3 +32,4 @@ from quagga.blocks.LastSelectorBlock import LastSelectorBlock
 from quagga.blocks.ParameterContainer import ParameterContainer
 from quagga.blocks.RowSlicingBlock import RowSlicingBlock
 from quagga.blocks.RepeatBlock import RepeatBlock
+from quagga.blocks.ListSlicingBlock import ListSlicingBlock
