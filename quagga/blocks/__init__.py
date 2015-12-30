e75fe15e647bec0b0e21d21d93c62e5fae25d9ba
sergii-gavrylov <sergii.gavrylov@grammarly.com>
2015-12-30 16:27:57 +0200

    add ArgmaxBlock :moyai:

--- quagga/blocks/__init__.py
+++ quagga/blocks/__init__.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ----------------------------------------------------------------------------
+from quagga.blocks.ArgmaxBlock import ArgmaxBlock
 from quagga.blocks.ColSlicingBlock import ColSlicingBlock
 from quagga.blocks.DotBlock import DotBlock
 from quagga.blocks.DropoutBlock import DropoutBlock
