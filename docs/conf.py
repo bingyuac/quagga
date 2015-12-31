fa48c872d1220f86bfa0108a6bc8ced87606dd41
sergii-gavrylov <sergii.gavrylov@grammarly.com>
2015-12-31 15:02:06 +0200

    try one more time

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@ import shlex
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
+os.environ["PYTHONPATH"] = "/home/docs/checkouts/readthedocs.org/user_builds/quagga/checkouts/latest/:/home/docs/checkouts/readthedocs.org/user_builds/quagga"
 
 # -- General configuration ------------------------------------------------
 
